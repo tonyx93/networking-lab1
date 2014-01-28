0cca0dea4b542c6acb7a6ad20760904ffa0d4eee
Tony Xu <txu0393@gmail.com>
2014-01-27 22:34:31 -0500

    Still buggy. It won't work more than once

--- webserver.c
+++ webserver.c
@@ -11,7 +11,7 @@
 #include <sys/stat.h>
 
 #define PORT 3456    //Specifies which port to listen on
-#define MSGLEN 1024   //Max message length to be sent at a time
+#define MSGLEN 2048   //Max message length to be sent at a time
 #define QUELEN 10    //Max queue length of connecting clients
 
 int main()
@@ -60,33 +60,38 @@ int main()
       printf("%s:%d connected\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 
       /* BEGIN HTTP */
- 
-      char *line;
+      
       char *word;
-      recv(clientfd, buffer, MSGLEN, 0);      // read client request
-      //printf(buffer);
-      line = strtok(buffer,"\n"); //Get request line
-      word = strtok(line," \r"); //Parse the method type
+      char *line;
+      recv(clientfd,buffer,MSGLEN,0);      // read client request
+      printf("Client request:\n%s\n",buffer);
+      word = strtok(buffer," "); //Parse the method type
       if(strcmp(word,"GET")==0) //GET
 	{
 	  char *res;
 	  char *version;
-	  res = strtok(line," \r"); //Parse resource
-	  version = strtok(line," \r");//Parse version
-	  line = strtok(buffer,"\n"); //Parse Response Headers
+	  res = strtok(NULL," "); //Parse resource
+	  version = strtok(NULL,"\n");//Parse version
+	  strtok(NULL,"\n");
+	  line = strtok(NULL,"\n"); //Parse Response Headers
 	  while(strlen(line)>1) //Will exit loop when line is an empty line
 	    {
 	      //I don't know what I'm supposed to do with the headers
-	      line = strtok(buffer,"\n");
+	      //line contains the header information so manipulate that string
+	      //printf("%s\n",line);
+	      line = strtok(NULL,"\n");
 	    }
 	  //At this point, buffer contains the optional message body
-
+	  
 	  //Server response for GET
-	  FILE *file;
-	  file = fopen(strcat(".",res),"r");
-	  char buf2[1000]; //Another buffer for storing bytes
+	  char buf2[256]; //Another buffer for storing bytes
+	  char *directory;
 	  struct tm timeinfo; //For time stuff
 	  time_t rawtime;
+	  buf2[0] = '.';
+	  FILE *file;
+	  directory = strcat(buf2,res);
+	  file = fopen(directory,"r");
 	  if(file==NULL)
 	    {
 	      sprintf(buffer,"HTTP/1.1 404 Not Found\r\n"); //File not found
@@ -102,7 +107,7 @@ int main()
 	  else
 	    {
 	      struct stat fstat; //Get file stats
-	      stat(strcat(".",res),&fstat);
+	      stat(directory,&fstat);
 
 	      sprintf(buffer,"HTTP/1.1 200 OK\r\n"); //Status Line
 	      strcat(buffer,"Date: "); //Date header
@@ -115,7 +120,7 @@ int main()
 	      timeinfo = *gmtime(&(fstat.st_mtime));
 	      strftime(buf2,sizeof(buf2), "%a, %d %b %Y %H:%M:%S %Z",&timeinfo);
 	      strcat(buffer,buf2);
-	      strcat(buffer,"/r/n");
+	      strcat(buffer,"\r\n");
 	      strcat(buffer,"Content-Type: text/html; charset=UTF-8\r\n"); //Content Type header
 	      strcat(buffer,"Content-Length: "); //Content Length header
 	      sprintf(buf2,"%lld",fstat.st_size);
@@ -125,13 +130,20 @@ int main()
 	      
 	      fread(buf2,1,fstat.st_size,file);
 	      strcat(buffer,buf2);
+	      fclose(file);
 	    }
 	  send(clientfd, buffer, MSGLEN, 0);
+	  printf("Server response:\n%s\n",buffer);
+
+	  //Client should reply with a Connection: close message
+	  recv(clientfd, buffer, MSGLEN, 0);      // read client request
+	  printf("Client response:\n%s\n",buffer);
 	}
       /* END HTTP */
       
       //Close connection
       close(clientfd);
+      printf("%s:%d disconnected\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
     }
 
   //Clean up
